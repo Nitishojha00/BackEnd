d9cefb4fe8417d5982b6a6c0ae22f633a2565ca8
Nitishojha00 <nitishojha00@gmail.com>
2025-10-19 00:58:41 +0530

    updated till lecture 03

---
+++ Day-02/Calculator/index.js
@@ -0,0 +1,6 @@
+const sum = require('./add')
+const sub = require('./first')
+const mul = require('./second')
+
+
+module.exports = {sum,sub,mul};
---
+++ Day-02/Main.js
@@ -0,0 +1,6 @@
+const {sum,sub,mul} = require('./Calculator');
+// index.js ko automatically lega ye node ki property
+
+sum(2,3);
+sub(4,5);
+mul(1,1);
