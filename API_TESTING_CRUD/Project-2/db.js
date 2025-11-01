16ce90baa8af56ae9b6d16e87aafbedfc175315c
Nitishojha00 <nitishojha00@gmail.com>
2025-11-01 06:50:54 +0530

    remocve db  api

--- API_TESTING_CRUD/Project-2/db.js
+++ API_TESTING_CRUD/Project-2/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 async function main()
 {
-    await mongoose.connect("mongodb+srv://nitishojha00:Bsndojhaji%4000@cluster0.5kohvit.mongodb.net/Project_2");
+    await mongoose.connect("");
 }
 
 module.exports = main;
--- API_TESTING_CRUD/RESTRAUNT_API/db.js
+++ API_TESTING_CRUD/RESTRAUNT_API/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 async function main(){
-     await mongoose.connect("mongodb+srv://nitishojha00:Bsndojhaji%4000@cluster0.5kohvit.mongodb.net/Restaurant_Management_System?retryWrites=true&w=majority");
+     await mongoose.connect("");
 
 }
 
